aca260035e50a885bd6d34700a2ffd494a464c96
Valission <FernandomvHull@gmail.com>
2025-05-20 14:49:04 -0400

    darkmode/lightmode

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let AButton = document.getElementById('AButton')
 let heading = document.querySelector('h4')
 let body = document.querySelector('body')
-let writing 
+let lighting = document.getElementById('switch') 
+let para = document.getElementById('para')
+let darkmode = false
 
 function makeAwsome(){
     heading.innerHTML = "<h4> This website is so awsome</h4>"
@@ -13,5 +15,20 @@ function makeAwsomer(){
     soCool.textContent = 'It"s so cool'
     heading.append(soCool)
 }
+function changemode(){
+    if (darkmode == false){
+        lighting.textContent = "Light mode"
+        body.style.backgroundColor = 'black'
+        para.style.color = 'aliceblue'
+        darkmode = true
+    }
+    else{
+        lighting.textContent = "Dark mode"
+        body.style.backgroundColor = 'white'
+        para.style.color = "black"
+        darkmode = false
+    }
+}
 AButton.addEventListener('click', makeAwsome)
 heading.addEventListener('click', makeAwsomer)
+lighting.addEventListener('click',changemode)
