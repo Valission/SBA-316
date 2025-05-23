996a7d4080c7e73ec8c31df6e45b521e730aedc3
Valission <FernandomvHull@gmail.com>
2025-05-22 21:46:23 -0400

    addedOpinonForm

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let heading = document.querySelector('h4')
 let body = document.querySelector('body')
 let lighting = document.getElementById('switch') 
 let para = document.getElementById('para')
+let question = document.getElementById('Qpara')
+let opinion = document.getElementById('opinion')
 let darkmode = false
 
 function makeAwsome(){
@@ -13,19 +15,23 @@ function makeAwsome(){
 function makeAwsomer(){
     soCool = document.createElement('p')
     soCool.textContent = 'It"s so cool'
-    heading.append(soCool)
+    heading.appendChild(soCool)
 }
 function changemode(){
     if (darkmode == false){
         lighting.textContent = "Light mode"
         body.style.backgroundColor = 'black'
         para.style.color = 'aliceblue'
+        opinion.style.backgroundColor = 'aliceblue'
         darkmode = true
     }
     else{
         lighting.textContent = "Dark mode"
         body.style.backgroundColor = 'white'
         para.style.color = "black"
+        question.style.color = 'black'
+        opinion.style.color = 'black'
+        opinion.style.border= '2px, solid, black'
         darkmode = false
     }
 }
